6155da0e301018abc5606c2ec2d5d4ae6c7df05b
carflo <carlos.flores@vacasa.com>
2020-12-08 18:17:02 -0800

    wip

--- phrases/counter.go
+++ phrases/counter.go
@@ -6,6 +6,7 @@ import (
 )
 
 var (
+	newregex                = regexp.MustCompile(`[^\w]+`)
 	nonAlphabeticRegex      = regexp.MustCompile("[^a-zA-Z ]+") // Remove non alphabetic chars
 	newLineRegex            = regexp.MustCompile(`\r?\n`)       // Remove CR and LF
 	doubleNewLineRegex      = regexp.MustCompile(`\r?\n\r?\n`)  // Remove LineBreaks
@@ -21,7 +22,7 @@ type phraseCounter struct {
 }
 
 func (p *phraseCounter) run() map[string]int {
-	sanitized := p.sanitize()
+	sanitized := p.sanitize2()
 	parsed := p.parse(sanitized)
 	p.count(parsed)
 
@@ -40,6 +41,27 @@ func (p *phraseCounter) parse(sanitized string) <-chan string {
 		close(result)
 	}()
 	return result
+}
+
+func (p *phraseCounter) parse2(sanitized string) <-chan string {
+	result := make(chan string)
+	words := strings.Split(sanitized, string(" "))
+
+	go func() {
+		for i := p.offset; i < len(words)-2; i = i + 3 {
+			phrase := strings.Join([]string{words[i], words[i+1], words[i+2]}, " ")
+			result <- phrase
+		}
+		close(result)
+	}()
+	return result
+}
+
+func (p *phraseCounter) sanitize2() string {
+	lowercase := strings.ToLower(string(p.originalText))
+	sanitized := newregex.ReplaceAllString(lowercase, " ")
+
+	return sanitized
 }
 
 func (p *phraseCounter) sanitize() string {
